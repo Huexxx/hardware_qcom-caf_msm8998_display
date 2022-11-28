da2e60b11ed10c9fcf21ec866b87036c289e002c
Huexxx <huexxx@gmail.com>
2022-11-28 15:31:03 +0100

    Revert "hwc2: use _exit(1) in place of abort()"
    This reverts commit 1ecdbb02ada917ddf110120a14884d994605808c.

--- sdm/libs/hwc2/hwc_session.cpp
+++ sdm/libs/hwc2/hwc_session.cpp
@@ -1582,8 +1582,8 @@ int HWCSession::HotPlugHandler(bool connected) {
         // This cannot be avoided due to SurfaceFlinger design
         // limitation in Android P.
         HWCDisplayExternal::Destroy(hwc_display_[HWC_DISPLAY_PRIMARY]);
-        DLOGE("External display is connected. Exit!!");
-        _exit(1);
+        DLOGE("External display is connected. Abort!!");
+        abort();
       }
       break;
     }
